86128612b2ee94a21513cc3c8fc234cab6b2bc6d
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-07-17 03:34:59 +0900

    feat: Exclude image serving endpoints from ApiResponseAdvice wrapping

--- src/main/kotlin/com/conymind/backend/config/advice/ApiResponseAdvice.kt
+++ src/main/kotlin/com/conymind/backend/config/advice/ApiResponseAdvice.kt
@@ -1,18 +1,32 @@
 package com.conymind.backend.config.advice
 
+import com.conymind.backend.controller.ImageController
 import com.conymind.backend.model.ApiResponse
 import org.springframework.core.MethodParameter
+import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+import java.lang.reflect.ParameterizedType
 
 @RestControllerAdvice
 class ApiResponseAdvice : ResponseBodyAdvice<Any> {
 
     override fun supports(returnType: MethodParameter, converterType: Class<out HttpMessageConverter<*>>): Boolean {
+        if (returnType.parameterType == ResponseEntity::class.java) {
+            val genericType = returnType.getGenericParameterType()
+            if (genericType is ParameterizedType) {
+                val typeArguments = genericType.actualTypeArguments
+                if (typeArguments.isNotEmpty() && typeArguments[0] == Resource::class.java) {
+                    return false
+                }
+            }
+        }
+
         return true
     }
 
